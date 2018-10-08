dcd0aaa21699b3268f23b867c98a39a498d607ea
Seth Charles Palileo <smpalileo@up.edu.ph>
2018-10-08 14:16:48 +0800

    Updated login and register logic

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const app = express()
+const bcrypt = require('bcrypt-nodejs');
 var mongoose = require('mongoose');
+var popup = require('popups');
 
 //Set up default mongoose connection
 var mongoDB = 'mongodb://127.0.0.1/my_database';
@@ -20,33 +22,87 @@ app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //use User schema located in models/User.js
-var User = require('./models/User');
+var Users = require('./models/User');
 
 app.get('/', function (req, res) {
   res.render('index');
 })
 
 //post function for new users
-app.post('/register', function (req, res) {
-	//creation of new User using data from form
-	var newUser = new User({
-		username: req.body.user,
-		password: req.body.pw,
-		email: req.body.email
-	});
-
-	//saving new user in MongoDB
-	newUser.save(function(err) {
+app.post('/', function (req, res, cb) {
+	//checks if username and email is already in the db
+	var app = res;
+	Users.find({ username: req.body.user }, function(err, user) {
 		if (err) throw err;
-		console.log("User saved successfully");
+			if (user.length) {
+				// console.log("Username already exists");
+				app.redirect(req.get('referer'));
+				popup.alert({
+				    content: "Username already exists"
+				});
+				return null;
+		}
+		else{
+			Users.find({ email: req.body.email }, function(err, user) {
+		  		if (err) throw err;
+				if (user.length) {
+					// console.log("Email already exists");
+					app.redirect(req.get('referer'));
+					popup.alert({
+				    	content: "Email already exists"
+					});
+					return null;
+				}
+				else{
+					//creation of new User using data from form
+					var newUser = new Users({
+						username: req.body.user,
+						password: req.body.pw,
+						email: req.body.email
+					});
+					//saving new user in MongoDB
+					newUser.save(function(err) {
+						if (err) throw err;
+						popup.alert({
+				    		content: "User saved successfully"
+						});
+					});
+					app.render('index');
+				}
+			});
+		}
 	});
-	res.render('index');
 })
 
-app.post('/login', function (req, res) {
-  // console.log(req.body.login);
-  // console.log(req.body.loginpw);
-  res.render('index');
+app.post('/login', function (req, res, cb) {
+	//checks if username exists in db
+	var app = res;
+  	Users.find({ username: req.body.login }, function(err, user) {
+		if (err) throw err;
+		//if username is not found
+		if (!user.length) {
+	  		popup.alert({
+	    		content: "User does not exist"
+			});
+			app.redirect(req.get('referer'));
+	    	return null;
+		}
+		else{
+			//if username is found, hashed password is decrypted and compared
+		    bcrypt.compare(req.body.loginpw, user[0].password, function(err, res) {
+				if(res) {
+					//if password is correct, redirects to another page
+					console.log("Logged in");
+					app.render('login');
+				} else {
+					popup.alert({
+			    		content: "Password incorrect"
+					});
+					app.redirect(req.get('referer'));
+				} 
+		    });
+		}
+		});
 })
 
 app.listen(3000, function () {
--- models/User.js
+++ models/User.js
@@ -4,40 +4,28 @@ var SALT_WORK_FACTOR = 10;
 var Schema = mongoose.Schema;
 
 var validateEmail = function(email) {
-
     var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
     return re.test(email)
-
 };
 
 var userSchema = new Schema({
-
   username: { type: String,  required: [true, 'Username must be provided'] },
-
   email:    { 
-    
     type: String,     
-
     Required:  'Email address cannot be left blank.',
-    
     validate: [validateEmail, 'Please fill a valid email address'],
          match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
     index: {unique: true, dropDups: true}
-    
     },
-
   	password: { type: String , required: [true,  'Password cannot be left blank']},
 
 });
 
 userSchema.pre('save', function(next) {
     var user = this;
-
     // generate a salt
     bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
         if (err) return next(err);
-
         // hash the password using our new salt
         bcrypt.hash(user.password, salt, null, function(err, hash) {
             if (err) return next(err);
@@ -47,12 +35,5 @@ userSchema.pre('save', function(next) {
         });
     });
 });
-
-userSchema.methods.comparePassword = function(candidatePassword, cb) {
-    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-        if (err) return cb(err);
-        cb(null, isMatch);
-    });
-};
 
 module.exports = mongoose.model('Users', userSchema);
