78e5c498e60a2c4efa1a847b510f33fbf2f9bcf1
Seth Charles Palileo <spalileo20181740@ad.acquireap>
2018-11-12 22:20:24 +0800

    reworked hashing

--- index.js
+++ index.js
@@ -116,8 +116,7 @@ app.post('/reset', function (req, res){
 				return res.status(404).send('Username does not exist in the database');
 			}
 			else{
-				console.log('It comes here');
-				user.findOneAndReplace({username: req.body.reset}, {password: 'asdafgasd'}, {new: false}, function(err) {
+				user.findOneAndReplace({username: req.body.reset}, {password: '1234'}, {new: false}, function(err) {
 					if (err) throw err;
 					console.log("Password reset successfully");
 					app.send('<h1>Welcome, ' + user[0].username + '</h1><br>' + 
